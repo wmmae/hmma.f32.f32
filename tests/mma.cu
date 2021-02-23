be81b91aa858bddba0a58e0eb4263d75c2d21bdf
mutsuki <mutsuki@momo86.net>
2021-02-23 15:17:14 +0900

    Add a test for D=AxB

--- tests/mma.cu
+++ tests/mma.cu
@@ -31,7 +31,7 @@ __device__ void fill_zero(float* const dst, const unsigned size) {
 }
 
 template <unsigned N, class T>
-__global__ void mma_kernel(float* const d_ptr, const float* const a_ptr, const float* const b_ptr, const float* const c_ptr) {
+__global__ void mma_kernel_abcd(float* const d_ptr, const float* const a_ptr, const float* const b_ptr, const float* const c_ptr) {
 	constexpr unsigned LD = 2 * N + 1;
 	__shared__ float smem[N * LD];
 	fill_zero(smem, N * LD);
@@ -64,6 +64,32 @@ __global__ void mma_kernel(float* const d_ptr, const float* const a_ptr, const f
 }
 
 template <unsigned N, class T>
+__global__ void mma_kernel_abd(float* const d_ptr, const float* const a_ptr, const float* const b_ptr) {
+	constexpr unsigned LD = 2 * N + 1;
+	__shared__ float smem[N * LD];
+	fill_zero(smem, N * LD);
+
+	mtk::wmma::fragment_f32<nvcuda::wmma::matrix_a, N, N, N, T, nvcuda::wmma::col_major> frag_a;
+	mtk::wmma::fragment_f32<nvcuda::wmma::matrix_b, N, N, N, T, nvcuda::wmma::col_major> frag_b;
+	mtk::wmma::fragment_f32<nvcuda::wmma::accumulator, N, N, N, T> frag_d;
+
+	// Load A
+	copy_matrix(smem, LD, a_ptr, N, N, N);
+	mtk::wmma::load_matrix_sync(frag_a, smem, LD);
+
+	// Load B
+	copy_matrix(smem, LD, b_ptr, N, N, N);
+	mtk::wmma::load_matrix_sync(frag_b, smem, LD);
+
+	// mma
+	mtk::wmma::mma_sync(frag_d, frag_a, frag_b);
+
+	// Store D
+	mtk::wmma::store_matrix_sync(smem, frag_d, LD, nvcuda::wmma::mem_col_major);
+	copy_matrix(d_ptr, N, smem, LD, N, N);
+}
+
+template <unsigned N, class T, bool AddC>
 void test_mma() {
 	float *hA, *hB, *hC, *hD;
 	cudaMallocHost(&hA, N * N * sizeof(float));
@@ -81,7 +107,10 @@ void test_mma() {
 	}
 	cudaDeviceSynchronize();
 
-	mma_kernel<N, T><<<1, warp_size>>>(hD, hA, hB, hC);
+	if (AddC)
+		mma_kernel_abcd<N, T><<<1, warp_size>>>(hD, hA, hB, hC);
+	else
+		mma_kernel_abd<N, T><<<1, warp_size>>>(hD, hA, hB);
 
 	cudaDeviceSynchronize();
 
@@ -92,13 +121,14 @@ void test_mma() {
 			for (unsigned k = 0; k < N; k++) {
 				cor_d += static_cast<double>(hA[k * N + m]) * static_cast<double>(hB[k + n * N]);
 			}
-			cor_d += hC[m + n * N];
+			if (AddC)
+				cor_d += hC[m + n * N];
 
 			max_error = std::max(max_error, std::abs(cor_d - hD[m + n * N]));
 		}
 	}
 
-	std::printf("[N = %2u, T = %4s] error = %e\n", N, get_type_name<T>().c_str(), max_error);
+	std::printf("[N = %2u, T = %4s, AddC = %u] error = %e\n", N, get_type_name<T>().c_str(), (AddC ? 1 : 0), max_error);
 
 	cudaFreeHost(hA);
 	cudaFreeHost(hB);
@@ -107,6 +137,8 @@ void test_mma() {
 }
 
 int main() {
-	test_mma<32, half>();
-	test_mma<32, nvcuda::wmma::precision::tf32>();
+	test_mma<32, half, true>();
+	test_mma<32, nvcuda::wmma::precision::tf32, true>();
+	test_mma<32, half, false>();
+	test_mma<32, nvcuda::wmma::precision::tf32, false>();
 }
