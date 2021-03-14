136813ba4fd12bad92e0077f82b3f30391699059
mutsuki <mutsuki@momo86.net>
2021-03-14 10:54:03 +0900

    Add `error_correction` to `Policy`

--- include/wmma_extension/detail/policy.hpp
+++ include/wmma_extension/detail/policy.hpp
@@ -5,13 +5,19 @@
 namespace mtk {
 namespace wmma {
 
+// Instruction policy
 struct op_mma;
 struct op_wmma;
 
+// Error correction policy
+struct op_with_error_correction;
+struct op_without_error_correction;
+
 namespace detail {
-template <class op_, int m_, int n_, int k_>
+template <class op_, class ec_, int m_, int n_, int k_>
 struct Policy {
 	using op = op_;
+	using error_correction = ec_;
 	static const int m = m_;
 	static const int n = n_;
 	static const int k = k_;
@@ -23,9 +29,9 @@ struct Policy {
 template <class T>
 struct default_policy;
 template <>
-struct default_policy<half                         > {using type = mtk::wmma::detail::Policy<mtk::wmma::op_wmma, 16, 16, 16>;};
+struct default_policy<half                         > {using type = mtk::wmma::detail::Policy<mtk::wmma::op_wmma, mtk::wmma::op_with_error_correction, 16, 16, 16>;};
 template <>
-struct default_policy<nvcuda::wmma::precision::tf32> {using type = mtk::wmma::detail::Policy<mtk::wmma::op_wmma, 16, 16, 8 >;};
+struct default_policy<nvcuda::wmma::precision::tf32> {using type = mtk::wmma::detail::Policy<mtk::wmma::op_wmma, mtk::wmma::op_with_error_correction, 16, 16, 8 >;};
 
 
 // ===================================
@@ -34,8 +40,8 @@ struct default_policy<nvcuda::wmma::precision::tf32> {using type = mtk::wmma::de
 template <class Use, class T, class Layout, class Policy>
 struct default_fragment;
 
-template <class Use, class T, class Layout, int fm, int fn, int fk>
-struct default_fragment<Use, T, Layout, Policy<op_wmma, fm, fn, fk>> {
+template <class Use, class T, class Layout, class ec, int fm, int fn, int fk>
+struct default_fragment<Use, T, Layout, Policy<op_wmma, ec, fm, fn, fk>> {
 	using type = nvcuda::wmma::fragment<Use, fm, fn, fk, T, Layout>;
 };
 } // namespace detail
