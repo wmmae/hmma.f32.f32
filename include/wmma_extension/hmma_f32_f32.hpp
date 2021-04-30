be997a0b7ccf8d010ac2664d175f22734a371c84
mutsuki <mutsuki@momo86.net>
2021-04-30 15:13:33 +0900

    Add arguments `converter_a` and `converter_b` to `load_vector` for op_(w)mma

--- include/wmma_extension/hmma_f32_f32.hpp
+++ include/wmma_extension/hmma_f32_f32.hpp
@@ -240,7 +240,11 @@ __device__ void load_vector(fragment<nvcuda::wmma::accumulator, m, n, k, T, void
 }
 
 template <class Use, int m, int n, int k, class T, class Layout, class Op, int fm, int fn, int fk>
-__device__ void load_vector(fragment<Use, m, n, k, T, Layout, mtk::wmma::mma_f32::Policy<Op, mtk::wmma::mma_f32::op_with_error_correction, fm, fn, fk>>& frag, const float* const ptr) {
+__device__ void load_vector(fragment<Use, m, n, k, T, Layout, mtk::wmma::mma_f32::Policy<Op, mtk::wmma::mma_f32::op_with_error_correction, fm, fn, fk>>& frag,
+		const float* const ptr,
+		const mtk::wmma::mma_f32::detail::Converter<typename mtk::wmma::detail::common::storage_t<typename mtk::wmma::mma_f32::detail::sub_frag_t<Use, T>::type>::type>& converter_a = typename mtk::wmma::mma_f32::detail::default_converter_A<T>::type{},
+		const mtk::wmma::mma_f32::detail::Converter<typename mtk::wmma::detail::common::storage_t<typename mtk::wmma::mma_f32::detail::sub_frag_t<Use, T>::type>::type>& converter_b = typename mtk::wmma::mma_f32::detail::default_converter_B<T>::type{}
+		) {
 	using Policy = mtk::wmma::mma_f32::Policy<Op, mtk::wmma::mma_f32::op_with_error_correction, fm, fn, fk>;
 	constexpr auto frag_m = mtk::wmma::mma_f32::detail::select_value<Use, Policy::m, Policy::k, Policy::m>::value;
 	constexpr auto frag_n = mtk::wmma::mma_f32::detail::select_value<Use, Policy::k, Policy::n, Policy::n>::value;
@@ -254,8 +258,8 @@ __device__ void load_vector(fragment<Use, m, n, k, T, Layout, mtk::wmma::mma_f32
 				for (unsigned bn = 0; bn < num_load_blocks; bn++) {
 					const auto mem_offset = mem_index + bn * vec_per_block;
 					const auto v = ptr[mem_offset];
-					const auto hv = mtk::wmma::detail::common::cast<T>(v);
-					const auto dhv = mtk::wmma::detail::common::cast<T>(detail::correction_scale_0<T>(v - mtk::wmma::detail::common::cast<float>(hv)));
+					const auto hv = converter_a(v);
+					const auto dhv = converter_b(v - mtk::wmma::detail::common::cast<float>(hv));
 					for (unsigned i = 0; i < frag_index_count; i++) {
 						const auto frag_index = frag_index_list[i];
 						frag.sub_frag  [bn * block_ld].x[frag_index] = hv ;
@@ -266,7 +270,11 @@ __device__ void load_vector(fragment<Use, m, n, k, T, Layout, mtk::wmma::mma_f32
 }
 
 template <class Use, int m, int n, int k, class T, class Layout, class Op, int fm, int fn, int fk>
-__device__ void load_vector(fragment<Use, m, n, k, T, Layout, mtk::wmma::mma_f32::Policy<Op, mtk::wmma::mma_f32::op_with_error_correction, fm, fn, fk>>& frag, const float* const ptr, const float mul) {
+__device__ void load_vector(fragment<Use, m, n, k, T, Layout, mtk::wmma::mma_f32::Policy<Op, mtk::wmma::mma_f32::op_with_error_correction, fm, fn, fk>>& frag,
+		const float* const ptr, const float mul,
+		const mtk::wmma::mma_f32::detail::Converter<typename mtk::wmma::detail::common::storage_t<typename mtk::wmma::mma_f32::detail::sub_frag_t<Use, T>::type>::type>& converter_a = typename mtk::wmma::mma_f32::detail::default_converter_A<T>::type{},
+		const mtk::wmma::mma_f32::detail::Converter<typename mtk::wmma::detail::common::storage_t<typename mtk::wmma::mma_f32::detail::sub_frag_t<Use, T>::type>::type>& converter_b = typename mtk::wmma::mma_f32::detail::default_converter_B<T>::type{}
+		) {
 	using Policy = mtk::wmma::mma_f32::Policy<Op, mtk::wmma::mma_f32::op_with_error_correction, fm, fn, fk>;
 	constexpr auto frag_m = mtk::wmma::mma_f32::detail::select_value<Use, Policy::m, Policy::k, Policy::m>::value;
 	constexpr auto frag_n = mtk::wmma::mma_f32::detail::select_value<Use, Policy::k, Policy::n, Policy::n>::value;
@@ -280,8 +288,8 @@ __device__ void load_vector(fragment<Use, m, n, k, T, Layout, mtk::wmma::mma_f32
 				for (unsigned bn = 0; bn < num_load_blocks; bn++) {
 					const auto mem_offset = mem_index + bn * vec_per_block;
 					const auto v = ptr[mem_offset] * mul;
-					const auto hv = mtk::wmma::detail::common::cast<T>(v);
-					const auto dhv = mtk::wmma::detail::common::cast<T>(detail::correction_scale_0<T>(v - mtk::wmma::detail::common::cast<float>(hv)));
+					const auto hv = converter_a(v);
+					const auto dhv = converter_b(v - mtk::wmma::detail::common::cast<float>(hv));
 					for (unsigned i = 0; i < frag_index_count; i++) {
 						const auto frag_index = frag_index_list[i];
 						frag.sub_frag  [bn * block_ld].x[frag_index] = hv ;
